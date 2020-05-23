7c7345d8d1b1a1d0954e3dc2855625a015998ffc
irrfan13 <irfan.khan@enquero.com>
2020-05-24 03:26:26 +0530

    user profile update by user id

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -14,6 +14,7 @@ const Dashboard = ({
   auth: { user },
   profile: { profile, loading }
 }) => {
+  console.log("profile:", profile);
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
--- main.go
+++ main.go
@@ -11,40 +11,46 @@ import (
 func main() {
 	r := mux.NewRouter()
 	// user regisration/login
-	// r.HandleFunc("/auth", controller.Auth).Methods("GET");
 	r.HandleFunc("/register", controller.RegisterHandler).Methods("POST")
 	r.HandleFunc("/login", controller.LoginHandler).Methods("POST")
+
 	// user profile
 	r.HandleFunc("/profile", controller.ProfileHandler).Methods("GET")
 	r.HandleFunc("/profiles", controller.GetProfiles).Methods("GET")
 	r.HandleFunc("/profile/{id}", controller.GetProfileByID).Methods("GET")
 	r.HandleFunc("/update-profile/{id}", controller.UpdateProfile).Methods("POST")
+
 	// jobs
 	r.HandleFunc("/jobs", controller.GetJobs).Methods("GET")
 	r.HandleFunc("/addjob", controller.CreateJobs).Methods("POST")
 	r.HandleFunc("/delete-job/{id}", controller.DeleteJobByID).Methods("DELETE")
 	r.HandleFunc("/update-job/{id}", controller.UpdateJob).Methods("PUT")
+
 	// applied, saved, rejected
 	r.HandleFunc("/savejob/{id}", controller.SaveJobs).Methods("POST")
 	r.HandleFunc("/rejectjob/{id}", controller.RejectedJobs).Methods("POST")
 	r.HandleFunc("/appliedjob/{id}", controller.AppliedJobs).Methods("POST")
+
 	// get applied, saved, rejected jobs
 	r.HandleFunc("/getsavedjobs/{id}", controller.GetSavedJobs).Methods("GET")
 	r.HandleFunc("/getappliedjobs/{id}", controller.GetAppliedJobs).Methods("GET")
 	r.HandleFunc("/getrejectedjobs/{id}", controller.GetRejectedJobs).Methods("GET")
+
 	// filterjobs
 	r.HandleFunc("/filterjobs", controller.FilterJobs).Methods("POST")
+
 	// upload files
 	r.HandleFunc("/upload-files", controller.UploadFile).Methods("POST")
 	r.HandleFunc("/fetchFiles", controller.FetchCVFiles).Methods("GET")
-	// r.HandleFunc("/download-files", controller.DownloadFile).Methods("GET")
+
 	// matchPercent
 	r.HandleFunc("/matchPercent/{userId}/{jobId}", controller.MatchPercent).Methods("GET")
-	// add/edit profile experience / education
 
+	// add/edit profile experience / education
 	r.HandleFunc("/create-profile", controller.AddProfileDetails).Methods("POST")
 	r.HandleFunc("/profile/experience", controller.AddProfileExperience).Methods("POST")
 	r.HandleFunc("/profile/education", controller.AddProfileEducation).Methods("POST")
+
 	r.HandleFunc("/protected", controller.TokenVerifyMiddleWare(controller.ProtectedEndPoint))
 	log.Fatal(http.ListenAndServe(":8080", r))
 
