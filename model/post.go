65c366e560510f8d2d52e5e436831325643cc40b
irrfan13 <irfan.khan@enquero.com>
2020-06-21 00:50:01 +0530

    post comments and likes model prepared

--- model/post.go
+++ model/post.go
@@ -8,12 +8,23 @@ import (
 
 //  Post ...
 type Post struct {
-	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	UID  primitive.ObjectID `json:"_uid,omitempty" bson:"_uid,omitempty"`
-	Post string             `json:"post,omitempty" bson:"post,omitempty"`
-	Date time.Time          `json:"date,omitempty" bson:"date,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UID      primitive.ObjectID `json:"_uid,omitempty" bson:"_uid,omitempty"`
+	Post     string             `json:"post,omitempty" bson:"post,omitempty"`
+	Date     time.Time          `json:"date,omitempty" bson:"date,omitempty"`
+	Comments []*Comment         `json:"comments,omitempty" bson:"comments,omitempty"`
+	Likes    []*Like            `json:"likes,omitempty" bson:"likes,omitempty"`
 }
 
 // Comment ...
 type Comment struct {
+	UID         primitive.ObjectID `json:"_uid,omitempty" bson:"_uid,omitempty"`
+	CommnetText string             `json:"commenttext,omitempty" bson:"commenttext,omitempty"`
+	Username    string             `json:"username,omitempty" bson:"username,omitempty"`
+	Date        time.Time          `json:"date,omitempty" bson:"date,omitempty"`
+}
+
+// Like ...
+type Like struct {
+	UID primitive.ObjectID `json:"_uid,omitempty" bson:"_uid,omitempty"`
 }
